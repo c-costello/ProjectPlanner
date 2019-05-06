400912451c4a28dd017110a0caf00296acf2f3ef
Clarice Costello <claricostello@outlook.com>
2019-05-06 15:31:59 -0700

    Created Planner DbContext

---
+++ ReactPlanner/ReactPlanner/Data/PlannerDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactPlanner.Data
+{
+    public class PlannerDbContext : DbContext
+    {
+        public PlannerDbContext(DbContextOptions<PlannerDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
