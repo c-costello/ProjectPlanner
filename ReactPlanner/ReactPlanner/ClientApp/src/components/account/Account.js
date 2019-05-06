18584e2ffc192704d88f54affdb6e33234d8963a
Clarice Costello <claricostello@outlook.com>
2019-05-06 16:54:14 -0700

    Built account constructor

---
+++ ReactPlanner/ReactPlanner/ClientApp/src/components/account/Account.js
@@ -0,0 +1,22 @@
+﻿import React, { Component } from 'react';
+
+export class Account extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            loading: true,
+            userData: [],
+            login: false,
+        }
+        fetch('api/Account/LoginCheck')
+            .then(response => response.json())
+            .then(data => this.setState({loading: false, login: data}))
+    }
+
+    render() {
+        return (
+            <div id="account-box">
+            </div>
+            )
+    }
+}
