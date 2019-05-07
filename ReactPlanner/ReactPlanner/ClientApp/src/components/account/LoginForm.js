796f4b97ff2b22a8b231ad86f943804eb728f7b6
Clarice Costello <claricostello@outlook.com>
2019-05-06 17:47:07 -0700

    Created LoginForm component and created login form

---
+++ ReactPlanner/ReactPlanner/ClientApp/src/components/account/LoginForm.js
@@ -0,0 +1,19 @@
+﻿import React, { Component } from 'react';
+
+export class LoginForm extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <form id="login-form">
+                <label>UserName </label>
+                <input name="Username" required />
+                <label> Password </label>
+                <input name="Password" type="password" required />
+                <button type="submit">Login</button>
+            </form>
+            )
+    }
+}
