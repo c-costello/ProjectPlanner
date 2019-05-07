ea08b6ee63cc7cad08c97340e045a5803fa3c709
Clarice Costello <claricostello@outlook.com>
2019-05-07 16:39:37 -0700

    Created Project View Model

---
+++ ReactPlanner/ReactPlanner/Models/ViewModels/ProjectViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactPlanner.Models.ViewModels
+{
+    public class ProjectViewModel
+    {
+        public Project.ColorType Color { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime FinalDueDate { get; set; }
+    }
+}
