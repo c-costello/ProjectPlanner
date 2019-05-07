a11ac9258494061f5d38f3496b52613bf1e39568
Clarice Costello <claricostello@outlook.com>
2019-05-07 16:56:48 -0700

    Created Project service with constructor

---
+++ ReactPlanner/ReactPlanner/Models/Services/ProjectService.cs
@@ -0,0 +1,19 @@
+﻿using ReactPlanner.Data;
+using ReactPlanner.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactPlanner.Models.Services
+{
+    public class ProjectService : IProject
+    {
+        private readonly PlannerDbContext _context;
+
+        public ProjectService(PlannerDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
