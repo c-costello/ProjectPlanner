27693756150cc1b8d0b90d0846906329085c6491
Clarice Costello <claricostello@outlook.com>
2019-05-07 17:37:55 -0700

    Created sendData method in CreatePorject component

--- ReactPlanner/ReactPlanner/ClientApp/src/components/project/CreateProject.js
+++ ReactPlanner/ReactPlanner/ClientApp/src/components/project/CreateProject.js
@@ -5,8 +5,19 @@ export class CreateProject extends React.Component {
     constructor(props) {
         super(props)
     }
-
+    async sendData(project) {
+        const url = 'api/Project/CreateProject';
+        const formData = new FormData();
+        formData.append('Title', project.Title);
+        formData.append('Description', project.Description);
+        formData.append('Color', project.Color);
+        formData.append('FinalDueDate', project.FinalDueDate);
+        await fetch(url, {
+            method: "POST",
+            body: formData
+        }).then(response => response.json).then(data => console.log(data));
+    }
     render() {
-        return ();
+        return (<ProjectForm ProjectData={this.sendData}/>);
     }
 }
