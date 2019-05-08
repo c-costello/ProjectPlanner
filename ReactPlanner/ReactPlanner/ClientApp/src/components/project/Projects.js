e9192d26b567f40641495d78fb3643ddb87c81fb
Clarice Costello <claricostello@outlook.com>
2019-05-07 17:12:18 -0700

    Created project component

---
+++ ReactPlanner/ReactPlanner/ClientApp/src/components/project/Projects.js
@@ -0,0 +1,11 @@
+﻿import React, { Component } from 'react';
+
+export class Projects extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return ();
+    }
+}
