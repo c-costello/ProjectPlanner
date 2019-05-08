c3d91a7ab99f4110ee4513dbe0bbf9e0f89b5db2
Clarice Costello <claricostello@outlook.com>
2019-05-07 17:22:21 -0700

    Created ProjectForm w/ constructor

---
+++ ReactPlanner/ReactPlanner/ClientApp/src/components/project/ProjectForm.js
@@ -0,0 +1,11 @@
+﻿import React, { Component } from 'react';
+
+export class ProjectForm extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return()
+    }
+}
