81cb04dbd62dec9adde7a18b452ed918fff6f82f
Clarice Costello <claricostello@outlook.com>
2019-05-07 18:46:55 -0700

    Built out DeleteProject component

--- ReactPlanner/ReactPlanner/ClientApp/src/components/project/DeleteProject.js
+++ ReactPlanner/ReactPlanner/ClientApp/src/components/project/DeleteProject.js
@@ -4,8 +4,16 @@ export class DeleteProject extends React.Component {
     constructor(props) {
         super(props)
     }
-
+    DeleteProject(id) {
+        const formData = new FormData();
+        formData.append('ID', id);
+        fetch('api/Project/DeleteProject', {
+            method: "Post",
+            body: formData,
+        }).then(response => response.json())
+            .then(data => console.log(data));
+    }
     render() {
-        return ();
+        return (<button onClick={this.DeleteProject}> Delete Project </button>);
     }
 }
