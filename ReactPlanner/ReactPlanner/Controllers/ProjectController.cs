388f7fbcd10b534d474ffd8333c37f68189d980d
Clarice Costello <claricostello@outlook.com>
2019-05-07 17:42:31 -0700

    Created empty ProjectController

---
+++ ReactPlanner/ReactPlanner/Controllers/ProjectController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReactPlanner.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactPlanner.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProjectController : Controller
+    {
+        private readonly IProject _context;
+
+        public ProjectController(IProject context)
+        {
+            _context = context;
+        }
+    }
+}
