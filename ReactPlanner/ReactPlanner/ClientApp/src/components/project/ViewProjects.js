475ce5ecd15da63f7e798d5e02d4c848ac46368c
Clarice Costello <claricostello@outlook.com>
2019-05-07 18:08:54 -0700

    Added fetch call to Get Projects, added jsx to render project data

--- ReactPlanner/ReactPlanner/ClientApp/src/components/project/ViewProjects.js
+++ ReactPlanner/ReactPlanner/ClientApp/src/components/project/ViewProjects.js
@@ -3,9 +3,28 @@
 export class ViewProjects extends React.Component {
     constructor(props) {
         super(props)
+        this.state = { projects: [], loading: true };
+        fetch('api/Project/GetProjects')
+            .then(response => response.json())
+            .then(data => { this.setState({ projects: data, loading: false }) });
     }
-
+    renderProject(projects) {
+        return (
+            <div>
+            <h3> Current Projects </h3>
+            {projects.map(project => 
+                <div>
+                    <p><span>Title:</span> {project.Title} </p>
+                    <p><span>Description:</span> {project.Description} </p>
+                    <p><span>Final Due Date:</span> {project.FinalDueDate} </p>
+                    <p><span>ID:</span> {project.ID} </p>
+                </div>                    
+                )}
+            </div>
+        )
+    };
     render() {
-        return ();
+        let contents = ViewProject.renderProject(this.state.projects);
+        return {contents};
     }
 }
