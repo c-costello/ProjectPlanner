476208e9f1cb2117d5466c577558ee53651b002b
Clarice Costello <claricostello@outlook.com>
2019-05-07 17:57:49 -0700

    Created ViewProject component w/constructor

---
+++ ReactPlanner/ReactPlanner/ClientApp/src/components/project/ViewProjects.js
@@ -0,0 +1,11 @@
+﻿import React, { Component } from 'react';
+
+export class ViewProjects extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return ();
+    }
+}
