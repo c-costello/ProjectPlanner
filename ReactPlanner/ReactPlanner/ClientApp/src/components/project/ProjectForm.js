6da8085c2f23db6637b8b1574214a7d95a4f5ae7
Clarice Costello <claricostello@outlook.com>
2019-05-07 17:24:51 -0700

    Created simple project form

--- ReactPlanner/ReactPlanner/ClientApp/src/components/project/ProjectForm.js
+++ ReactPlanner/ReactPlanner/ClientApp/src/components/project/ProjectForm.js
@@ -4,8 +4,24 @@ export class ProjectForm extends React.Component {
     constructor(props) {
         super(props)
     }
-
+    const form = (
+        <form>
+            <label>Title</label>
+            <input name="title" required />
+            <label>Description</label>
+            <input name="description" type="text" required />
+            <label> Final Due Date </label>
+            <input name="finalDueDate" type="date" required />
+            <label>Color</label>
+            <select name="Color">
+                <option value="0"> Blue </option>
+                <option value="1"> Green </option>
+                <option value="2"> Yellow </option>
+                <option value="3"> Orange </option>
+            </select>
+        </form>
+        )
     render() {
-        return()
+        return ();
     }
 }
